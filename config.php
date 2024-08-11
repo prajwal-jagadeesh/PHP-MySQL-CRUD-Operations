4d21a04ef6154638dd6b81c6c9ca22d6aa7fb189
prajwal-jagadeesh <156173951+prajwal-jagadeesh@users.noreply.github.com>
2024-08-11 19:47:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
 mysqli_report(MYSQLI_REPORT_OFF);
 
-# Database credentials
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "crud");
+// Database credentials from environment variables
+define("DB_SERVER", getenv('DB_HOST'));
+define("DB_USERNAME", getenv('DB_USER'));
+define("DB_PASSWORD", getenv('DB_PASSWORD'));
+define("DB_NAME", getenv('DB_NAME'));
 
-# Create connection
+// Create connection
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-# Check connection
+// Check connection
 if (!$link) {
-  echo "Connection error: " . mysqli_connect_error();
+    die("Connection error: " . mysqli_connect_error());
 }
+?>
