e195e2eee8789ed7b6f1643d4bb89765e93853c7
prajwal-jagadeesh <156173951+prajwal-jagadeesh@users.noreply.github.com>
2024-08-11 20:35:59 +0530

    Update create.php

--- create.php
+++ create.php
@@ -1,23 +1,4 @@
 <?php
-mysqli_report(MYSQLI_REPORT_OFF);
-
-// Database credentials from environment variables
-define("DB_SERVER", getenv('DB_HOST'));
-define("DB_USERNAME", getenv('DB_USER'));
-define("DB_PASSWORD", getenv('DB_PASSWORD'));
-define("DB_NAME", getenv('DB_NAME'));
-
-// Create connection
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// Check connection
-if (!$link) {
-    die("Connection error: " . mysqli_connect_error());
-}
-?>
-
-ubuntu@ip-172-31-44-242:~/PHP-MySQL-CRUD-Operations$ cat create.php 
-<?php
 # Include connection
 require_once "./config.php";
 
